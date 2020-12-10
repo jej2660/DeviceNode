1e67f0d73ac28e028681b4084ff78225265d9df8
SeokJiwon <tjrwldnjs103@naver.com>
2020-12-10 20:10:23 +0900

    fix class-method tbl

--- makeflowmap.py
+++ makeflowmap.py
@@ -18,21 +18,23 @@ from apkanalyzer import Apkanlyzer
 class TableMaker:
     def __init__(self):
         self.flow_tbl={}
+        self.getLogger()
 
     def getLogger(self):
-        __log = logging.getLogger("TraceFlow")
+        __log = logging.getLogger("TableMaker")
         __log.setLevel(logging.DEBUG)
         stream_hander = logging.StreamHandler()
         stream_hander.setLevel(logging.DEBUG)
         __log.addHandler(stream_hander)
-        file_handler = logging.FileHandler('my.log')
-        file_handler.setLevel(logging.CRITICAL)
-        __log.addHandler(file_handler)
+        #file_handler = logging.FileHandler('my.log')
+        #file_handler.setLevel(logging.CRITICAL)
+        #__log.addHandler(file_handler)
         self.logger = __log
         return __log
 
-    def xref_tbl(self, dx):
+    def method_xref(self, dx):
         self.flow_tbl["xref"]={}
+        self.logger.critical("-----XREF TBL START-----")
         for p_method in dx.get_methods():
             key=self.extract_class_name(str(p_method.get_class_name()))+"::"+str(p_method.name)
             if p_method.is_android_api():
@@ -42,21 +44,65 @@ class TableMaker:
                 for c_method in p_method.get_xref_to():
                     value=self.extract_class_name(str(c_method[0].name))+"::"+str(c_method[1].name)
                     self.flow_tbl["xref"][key].append(value)
-        with open('flow_tbl.json', 'w') as f:
-            json.dump(self.flow_tbl,f)   
-        f.close()
-        self.logger.critical("Make XREF tbl Finished")
+        self.logger.critical("-----XREF TBL FINISH-----")
+
 
     def class_methods_tbl(self, dx):
         self.flow_tbl["class-methods"]={}
+        self.logger.critical("-----CLASS_METHODS TBL START-----")
         for c in dx.get_classes():
             if c.is_external() or c.is_android_api():
                 continue
             key_class=self.extract_class_name(str(c.name))
+            value_method=[]
             for method in c.get_methods():
-                value_method=[]
                 value_method.append(str(method.name))
                 self.flow_tbl["class-methods"][key_class]=value_method
+        self.logger.critical("-----CLASS_METHODS TBL FINISH-----")
+
+    """
+    def make_xref_root(self, dx,mainactivity):
+        root=self.extract_class_name(mainactivity)
+        main_activity="^"+FormatClassToJava(mainactivity)+"$"
+        for cls in dx.find_classes(main_activity):
+            self.xref_tbl[root]=[]
+            tmp=[]
+            for meth in cls.get_methods():
+                self.xref_tbl[root].append(str(meth.name))
+                tmp.append(meth)
+                #self.make_xref_node(dx,meth)
+            for m in tmp:
+                self.make_xref_node(dx,m)
+
+    def make_xref_node(self, dx, method):
+        key=self.extract_class_name(str(method.get_class_name()))+"::"+str(method.name)
+        tmp=[]
+        if self.check_method(method):
+            self.xref_tbl[key]=[]
+            for meth in method.get_xref_to():
+                value=self.extract_class_name(str(meth[0].name))+"::"+str(meth[1].name)
+                self.xref_tbl[key].append(value)
+                tmp.append(meth[1])
+                #self.make_xref_node(dx,meth[1])
+            for m in tmp:
+                self.make_xref_node(dx,m)
+        
+    def check_method(self, method):
+        if method.name in self.xref_tbl.keys():
+            #self.logger.critical("already exist in xref_tbl keys")
+            return False
+        if method.is_external():
+            #self.logger.critical("External")
+            return False
+        if method.is_android_api():
+            #self.logger.critical("Android API")
+            return False
+        if(str(method.name) == "<init>"):
+            #self.logger.critical("method name == <init>")
+            return False
+        else: 
+            return True
+    """
 
     def extract_class_name(self, dir_class):
         tmp=dir_class.split('/')
